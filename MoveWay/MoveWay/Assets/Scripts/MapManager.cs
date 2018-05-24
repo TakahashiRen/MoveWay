8c4b7d32339b2c676a03e8c62cbc970f0725de06
TakahashiRen <tkren7411@gmail.com>
2018-05-24 12:06:09 +0900

    クリア判定機能追加 タイルの枠線追加

--- MoveWay/MoveWay/Assets/Scripts/MapManager.cs
+++ MoveWay/MoveWay/Assets/Scripts/MapManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MapManager : MonoBehaviour
 {
@@ -59,6 +60,9 @@ public class MapManager : MonoBehaviour
     //プレイヤー
     GameObject player;
 
+    //親
+    public GameObject parent;
+
     //アイテム管理
     List<GameObject> items;
 
@@ -69,6 +73,12 @@ public class MapManager : MonoBehaviour
 
     //タイルを選んだ数
     int tileSelectCount;
+
+    //クリアしたかどうか
+    bool IsClear;
+
+    //クリアテキストデータ
+    public GameObject text;
 
     void Start()
     {
@@ -81,8 +91,8 @@ public class MapManager : MonoBehaviour
         for(int i = 0;i < parentMaxNum;i++)
         {
             //親の生成
-            gridParent[i] = new GameObject("GridParent" + i.ToString());
-
+            gridParent[i] = Instantiate(parent,transform.position,transform.rotation);
+            //"GridParent" + i.ToString()
             //親の位置を設定
             Vector3 pos = new Vector3();
             pos.x = ((-64 * 4.5f) + (i % 3) * 192);
@@ -120,6 +130,7 @@ public class MapManager : MonoBehaviour
                         obj = Instantiate(mapObject[1], new Vector3(pos.x, pos.y, 0.0f), transform.rotation);
                         player = Instantiate(mapObject[3], new Vector3(pos.x, pos.y, 0.0f), transform.rotation);
                         player.GetComponent<PlayerController>().SetMapPosition(i, j);
+                        baseMapData[i, j] = 5;
                         break;
                     case (int)mapObjectNum.Item:
                         obj = Instantiate(mapObject[1], new Vector3(pos.x, pos.y, 0.0f), transform.rotation);
@@ -139,6 +150,7 @@ public class MapManager : MonoBehaviour
         tileSelectCount = 0;
 
         player.GetComponent<PlayerController>().SetMapManager(this);
+        IsClear = false;
     }
 
     //更新処理
@@ -154,6 +166,12 @@ public class MapManager : MonoBehaviour
             tileSelectCount = 0;
         }
         IsGetItem();
+        IsClear = CheckClear();
+
+        if(IsClear)
+        {
+            text.GetComponent<Text>().text = "Clear!!";
+        }
     }
 
     private void CheckClickParent()
@@ -194,7 +212,12 @@ public class MapManager : MonoBehaviour
                 mapData[mapData1Y, mapData1X] = mapData[mapData2Y, mapData2X];
                 mapData[mapData2Y, mapData2X] = tmp;
 
-                if(playerX == mapData1X && playerY == mapData1Y)
+                int tmp2;
+                tmp2 = baseMapData[mapData1Y, mapData1X];
+                baseMapData[mapData1Y, mapData1X] = baseMapData[mapData2Y, mapData2X];
+                baseMapData[mapData2Y, mapData2X] = tmp2;
+
+                if (playerX == mapData1X && playerY == mapData1Y)
                 {
                     player.GetComponent<PlayerController>().SetMapPosition(mapData2X, mapData2Y);
                     player.GetComponent<PlayerController>().TranslateMapPosition();
@@ -281,5 +304,21 @@ public class MapManager : MonoBehaviour
                 player.GetComponent<PlayerController>().AddItemNum();
             }
         }
+    }
+
+
+    bool CheckClear()
+    {
+        for(int i = 0;i < 9;i++)
+        {
+            for(int j = 0;j < 9;j++)
+            {
+                if(ansMapData[i,j] != baseMapData[i,j])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }   
